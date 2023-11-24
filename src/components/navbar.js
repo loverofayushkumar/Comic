4f68643bdc5684947b75739e78f63fb0d5454bcc
loveofayushkumar <kumartuktuk118@gmail.com>
2023-11-24 21:39:46 +0530

    first commit

---
+++ src/components/navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Nav=()=>{
+    return(
+        <>
+        <nav>
+            <div className="navbar">
+                <ul className="nav_list">
+                    <li className="list">Home</li>
+                    <li className="list">Menu</li>
+                    <li className="list">Contact</li>
+                    <li className="list">About</li>
+                </ul>
+                <div className="logo">
+                    <img src="https://i.gifer.com/52O8.gif" alt="kk" />
+                </div>
+            </div>
+        </nav>
+        </>
+    )
+}
+
+
+export default Nav;
